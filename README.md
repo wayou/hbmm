b5ee638a63aa3b3b59078816c5cbb7cb4fd3b46f
wayou <liuwayong@gmail.com>
2019-02-21 10:33:34 +0800

    new origin and format update: - add ustc origin - code format - add name and homepage for each origin

--- README.md
+++ README.md
@@ -38,6 +38,11 @@ Following mirrors are included :
     - brew: `https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git`
     - core: `https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git`
 
+### TODO
+
+- [] customize origins (add, remove)
+- [] speed test
+
 
 ### License
 
--- cli.js
+++ cli.js
@@ -1,98 +1,95 @@
 #!/usr/bin/env node
-const { exec } = require('child_process');
-var program = require('commander');
-var mirrors = require('./mirrors.json');
-var PKG = require('./package.json');
+const { exec } = require("child_process");
+var program = require("commander");
+var mirrors = require("./mirrors.json");
+var PKG = require("./package.json");
+
+program.version(PKG.version);
 
 program
-    .version(PKG.version);
+  .command("ls")
+  .description("list all mirrors")
+  .action(onList);
 
 program
-    .command('ls')
-    .description('list all mirrors')
-    .action(onList);
+  .command("use <mirror>")
+  .description("switch to <mirror>")
+  .action(onUse);
 
 program
-    .command('use <mirror>')
-    .description('switch to <mirror>')
-    .action(onUse);
+  .command("help", { isDefault: true })
+  .description("Print this help")
+  .action(function() {
+    program.outputHelp();
+  });
 
-program
-    .command('help', { isDefault: true })
-    .description('Print this help')
-    .action(function () {
-        program.outputHelp();
-    });
-
-program
-    .parse(process.argv);
-
+program.parse(process.argv);
 
 if (process.argv.length === 2) {
-    program.outputHelp();
+  program.outputHelp();
 }
 
-/*//////////////// cmd methods /////////////////*/
-
 function onList() {
-    getCurrentRegistry(function (cur) {
-        var info = [''];
-        var allMirrors = mirrors;
+  getCurrentMirror(function(cur) {
+    var info = [""];
+    var allMirrors = mirrors;
 
-        Object.keys(allMirrors).forEach(function (key) {
-            var item = allMirrors[key];
-            var prefix = item.brew === cur ? '* ' : '  ';
-            info.push(prefix + key + line(key, 8) + item.brew);
-        });
-
-        info.push('');
-        printMsg(info);
+    Object.keys(allMirrors).forEach(function(key) {
+      var item = allMirrors[key];
+      var prefix = item.brew === cur ? "* " : "  ";
+      info.push(prefix + item.name + line(key, 8) + item.brew);
     });
 
+    info.push("");
+    printMsg(info);
+  });
 }
 
 function onUse(name) {
-    var allMirrors = mirrors;
-    if (allMirrors.hasOwnProperty(name)) {
-        var mirror = allMirrors[name];
-        runCommand(`cd "$(brew --repo)" && git remote set-url origin ${mirror.brew}`)
-        runCommand(`cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core" && git remote set-url origin ${mirror.core}`, () => {
-            printMsg([
-                '', '   Registry has been set to: ' + name, ''
-            ]);
-        })
-    } else {
-        printMsg([
-            '', name + ' not found!', 'Check the spell and try again.'
-        ]);
-    }
+  var allMirrors = mirrors;
+  if (allMirrors.hasOwnProperty(name)) {
+    var mirror = allMirrors[name];
+    runCommand(
+      `cd "$(brew --repo)" && git remote set-url origin ${mirror.brew}`
+    );
+    runCommand(
+      `cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core" && git remote set-url origin ${
+        mirror.core
+      }`,
+      () => {
+        printMsg(["", "   Homebrew origin has been set to: " + name, ""]);
+      }
+    );
+  } else {
+    printMsg(["", name + " not found!", "Check the spell and try again."]);
+  }
 }
 
-function getCurrentRegistry(cb) {
-    const command = 'cd "$(brew --repo)" && git config --get remote.origin.url';
-    runCommand(command, cb)
+function getCurrentMirror(cb) {
+  const command = 'cd "$(brew --repo)" && git config --get remote.origin.url';
+  runCommand(command, cb);
 }
 
 function runCommand(command, cb) {
-    exec(command, (err, stdout, stderr) => {
-        if (err) {
-            // node couldn't execute the command
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-        }
-        cb && cb(stdout.trim());
-    });
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      // node couldn't execute the command
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+    }
+    cb && cb(stdout.trim());
+  });
 }
 
 function printMsg(infos) {
-    infos.forEach(function (info) {
-        console.log(info);
-    });
+  infos.forEach(function(info) {
+    console.log(info);
+  });
 }
 
 function line(str, len) {
-    var line = new Array(Math.max(1, len - str.length)).join('-');
-    return ' ' + line + ' ';
+  var line = new Array(Math.max(1, len - str.length)).join("-");
+  return " " + line + " ";
 }
